8bbe00cb14a6920a1551aaff6a549c067498145d
JurBri <89058157+JurBri@users.noreply.github.com>
2024-09-01 06:44:12 +0000

    Se crea el proyecto de loggeo

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,100 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			user: null,
+			token: null,
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+
+			setToken: () => {
+				const token = sessionStorage.getItem('token')
+				const user = JSON.parse(sessionStorage.getItem('user'))
+				setStore({ ...getStore(), user: user, token: token })
 			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			createUser: async (email, password) => {
+				try {
+					const response = await fetch("https://legendary-train-q7v76477wr9rf467-3001.app.github.dev/api/register",
+						{
+							method: "POST",
+							headers: { "Content-type": "application/json" },
+							body: JSON.stringify({
+								"email": email,
+								"password": password,
+							})
+						})
+					if (!response.ok) {
+						console.log("Uh oh!", response.status)
+						return
+					}
+					const data = await response.json()
+					console.log("This is my data:", data)
+
+				} catch (error) {
+					console.log("Error", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
+
+			login: async (email, password) => {
+				try {
+					const response = await fetch("https://legendary-train-q7v76477wr9rf467-3001.app.github.dev/api/login/",
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json"
+							},
+							body: JSON.stringify({ email: email, password: password })
+						});
+
+					if (response.status !== 201) {
+						console.log("There has been some error");
+						return false;
+					}
+
+					const data = await response.json()
+					sessionStorage.setItem("token", data.token)
+					sessionStorage.setItem("user", JSON.stringify(data.user))
+					setStore({ ...getStore(), token: data.token, user: data.user })
+					return true
+
+				} catch (error) {
+					console.log("error catch:", error)
+				}
+			},
+
+			logout: async () => {
+				sessionStorage.removeItem("token");
+				sessionStorage.removeItem("user");
+				setStore({ ...getStore(), token: null, user: null })
+			},
+
+			privateInfo: async () => {
 				const store = getStore();
+				const opts = {
+					headers: {
+						Authorization: "Bearer " + store.token
+					}
+				};
+				try {
+					const response = await fetch("https://legendary-train-q7v76477wr9rf467-3001.app.github.dev/api/single", opts)
+					if (response.status !== 200) {
+						console.log("There has been some error");
+						return false;
+					}
+					const data = await response.json()
+					setStore({ ...getStore(), user: data.user })
+					return true
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				} catch (error) {
+					console.log("Not working as expected")
+				}
 			}
-		}
-	};
+
+		},
+
+
+	}
 };
+
 
 export default getState;
